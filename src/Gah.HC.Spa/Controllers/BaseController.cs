f1e71c84bdea31a853b7cfc761d0484d73a29923
Geoff Horsey <geoff.horsey@outlook.com>
2020-03-31 16:05:05 -0700

    feat: added version

---
+++ src/Gah.HC.Spa/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿namespace Gah.HC.Spa.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Class BaseController.
+    /// Implements the <see cref="Microsoft.AspNetCore.Mvc.Controller" />.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    public class BaseController : ControllerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        protected BaseController(ILogger logger)
+        {
+            this.Logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        /// <value>The logger.</value>
+        protected internal ILogger Logger { get; }
+    }
+}
---
+++ src/Gah.HC.Spa/Controllers/VersionController.cs
@@ -0,0 +1,42 @@
+﻿namespace Gah.HC.Spa.Controllers
+{
+    using System.Diagnostics;
+    using System.Net;
+    using Gah.HC.Domain;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Class VersionController.
+    /// </summary>
+    [ApiController]
+    [Route("versionz")]
+    public class VersionController : BaseController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VersionController" /> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public VersionController(ILogger<VersionController> logger)
+            : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Versions this instance.
+        /// </summary>
+        /// <returns>An IActionResult.</returns>
+        [HttpGet("")]
+        [ProducesResponseType(typeof(Result<string>), (int)HttpStatusCode.OK)]
+        public IActionResult Version()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fileVersion.FileVersion;
+
+            this.Logger.LogInformation(1, "Fetching version: {versionString}", version);
+
+            return this.Ok(version.MakeSuccessfulResult());
+        }
+    }
+}
