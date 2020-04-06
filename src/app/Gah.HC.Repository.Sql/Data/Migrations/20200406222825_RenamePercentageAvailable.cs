9ed3c8853320d4d8d0c2f1f6c1c16cb25b9701dc
Geoff Horsey <geoff.horsey@outlook.com>
2020-04-06 15:35:56 -0700

    feat: added migraton to rename property in the database

---
+++ src/app/Gah.HC.Repository.Sql/Data/Migrations/20200406222825_RenamePercentageAvailable.cs
@@ -0,0 +1,40 @@
+﻿namespace Gah.HC.Repository.Sql.Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc/>
+    public partial class RenamePercentageAvailable : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder = migrationBuilder ?? throw new ArgumentNullException(nameof(migrationBuilder));
+            migrationBuilder.RenameColumn(
+                name: "PercentageAvailable",
+                table: "Hospitals",
+                newName: "PercentOfUsage");
+
+            migrationBuilder.RenameColumn(
+                name: "PercentageAvailable",
+                table: "HospitalCapacity",
+                newName: "PercentOfUsage");
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder = migrationBuilder ?? throw new ArgumentNullException(nameof(migrationBuilder));
+
+            migrationBuilder.RenameColumn(
+                newName: "PercentageAvailable",
+                table: "Hospitals",
+                name: "PercentOfUsage");
+
+            migrationBuilder.RenameColumn(
+                newName: "PercentageAvailable",
+                table: "HospitalCapacity",
+                name: "PercentOfUsage");
+        }
+    }
+}
